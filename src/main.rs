659ee8975a99a6917c595bc13efe315993bd82bb
Amiya mandal <amiya19mandal@gmail.com>
2023-06-10 13:48:43 +0530

    updated code

--- src/main.rs
+++ src/main.rs
@@ -14,13 +14,13 @@ fn main() -> Result<(), Box<dyn Error>> {
     pin.set_low();
 
     // Blink the LED by setting the pin's logic level high for 500 ms.
-    loop {
-        pin.set_high();
-        println!("set pin high");
-        thread::sleep(Duration::from_millis(500));
-        println!("set pin low");
-        pin.set_low();
-    }
+    // loop {
+    //     pin.set_high();
+    //     println!("set pin high");
+    //     thread::sleep(Duration::from_millis(500));
+    //     println!("set pin low");
+    //     pin.set_low();
+    // }
 
     Ok(())
 }
